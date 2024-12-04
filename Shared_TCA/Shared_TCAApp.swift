6a0db76a7343b25307bc533c67e155c49dc4cf1d
Ivan Amidžić <iamidzic92@gmail.com>
2024-12-04 20:15:43 +0100

    Initial Commit

---
+++ Shared_TCA/Shared_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shared_TCAApp.swift
+//  Shared_TCA
+//
+//  Created by Ivan Amidzic on 04.12.2024..
+//
+
+import SwiftUI
+
+@main
+struct Shared_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
